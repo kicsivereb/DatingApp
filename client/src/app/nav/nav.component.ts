d154a3bf1392076fa7bfa9b9b16af9d00b9e7a97
Eva Fekete <eva@microtomsoftware.com>
2021-10-08 12:04:55 +0100

    End of section five

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent {
+  public model: any = {};
+
+  constructor(public accountService: AccountService) {}
+
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
